5c6e8e8ed88dab45de1de3cd3462d81c9b8a09f2
Arno Robeys <arno.robeys@student.ucll.be>
2023-04-18 23:03:17 +0200

    Verbetering settings page

--- frontend/chatoverflow/pages/user/settings.tsx
+++ frontend/chatoverflow/pages/user/settings.tsx
@@ -2,7 +2,7 @@ import { UserProfile } from '@/types/userprofile.type';
 import { UserCircleIcon } from '@heroicons/react/24/solid'
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { FormEventHandler, useEffect, useState } from 'react';
+import { FormEventHandler, useEffect, useRef, useState } from 'react';
 
 const userSettings: React.FC = () => {
 
@@ -17,7 +17,7 @@ const userSettings: React.FC = () => {
     const response = await fetch(`http://localhost:3000/user/${loggedInUserId}/profile`);
     var data = await response.json();
 
-    if(!data.profile) {
+    if(!data.hasOwnProperty('profile')) {
       const response2 = await fetch(`http://localhost:3000/profile/createprofile`, {
         method: 'POST',
         body: JSON.stringify({userid: parseInt(loggedInUserId)}),
@@ -30,10 +30,13 @@ const userSettings: React.FC = () => {
     setUser(data);
   })()},[]);
 
+  const inputRefs = Array.from({ length: 6 }).map(() => useRef<HTMLInputElement>(null));
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+
 
   const handleSubmit : FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    
+    const [firstnameInput, lastnameInput, nicknameInput, educationInput, workInput, tagsInput] = inputRefs.map((ref) => ref.current);
   };
 
   return (
@@ -58,36 +61,36 @@ const userSettings: React.FC = () => {
             <div className="mt-10 flex flex-col sm:flex-row sm:justify-between">
                     <div className="mb-4 sm:mb-0 sm:w-1/2 sm:pr-2">
                         <label htmlFor="firstname" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
-                        <input type="text" value={user?.firstname} name="firstname" id="firstname" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        <input type="text" defaultValue={user?.firstname} ref={inputRefs[0]} name="firstname" id="firstname" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                     </div>
                     <div className="sm:w-1/2 sm:pl-2">
                         <label htmlFor="lastname" className="block text-sm font-medium leading-6 text-gray-900">Lastname</label>
-                        <input type="text" value={user?.lastname} name="lastname" id="lastname" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        <input type="text" defaultValue={user?.lastname} ref={inputRefs[1]} name="lastname" id="lastname" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                     </div>
             </div>
             <div className="mt-4">
                 <label htmlFor="nickname" className="block text-sm font-medium leading-6 text-gray-900">Nickname</label>
-                <input type="text" defaultValue={user?.nickname ? user.nickname : ""} name="nickname" id="nickname" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <input type="text" defaultValue={user?.nickname || ""} ref={inputRefs[2]} name="nickname" id="nickname" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
             <div className="mt-4">
                 <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">Description</label>
-                <textarea name="description" defaultValue={user?.profile?.description ? user.profile.description : ""} id="description" placeholder="Write a few sentences about yourself" className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"/>
+                <textarea name="description" defaultValue={user?.profile.description || ""} ref={descriptionRef} id="description" placeholder="Write a few sentences about yourself" className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"/>
             </div>
 
             <div className="mt-4 flex flex-col sm:flex-row sm:justify-between">
                     <div className="mb-4 sm:mb-0 sm:w-1/2 sm:pr-2">
                         <label htmlFor="education" className="block text-sm font-medium leading-6 text-gray-900">Education</label>
-                        <input type="text" defaultValue={user?.profile?.education ? user.profile.education : ""} name="education" id="education" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        <input type="text" defaultValue={user?.profile.education || ""} ref={inputRefs[3]} name="education" id="education" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                     </div>
                     <div className="sm:w-1/2 sm:pl-2">
                         <label htmlFor="work" className="block text-sm font-medium leading-6 text-gray-900">Work</label>
-                        <input type="text" defaultValue={user?.profile?.work ? user.profile.work : ""} name="work" id="work" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        <input type="text" defaultValue={user?.profile.work || ""} ref={inputRefs[4]} name="work" id="work" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                     </div>
             </div>
 
             <div className="mt-4">
                 <label htmlFor="tags" className="block text-sm font-medium leading-6 text-gray-900">Tags</label>
-                <input type="text" defaultValue={user?.profile?.tags ? user.profile.tags : ""} name="tags" id="tags" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <input type="text" defaultValue={user?.profile.tags || ""} ref={inputRefs[5]} name="tags" id="tags" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
         </div>
 
