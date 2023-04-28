0ef1d268cad61e8eeeeb9e2915ab7e3a1a07e6ce
Gerald-Waerseggers <r0853509@ucll.be>
2023-04-28 10:20:54 +0200

    logo aangepast

--- frontend/chatoverflow/components/Navbar.tsx
+++ frontend/chatoverflow/components/Navbar.tsx
@@ -41,17 +41,17 @@ const Navbar: React.FC = () => {
                   <span className="sr-only">Open main menu</span>
                   {isOpen ? (
                     <><XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  <Transition as={Fragment} appear show={isOpen}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95">
-                    <Disclosure.Panel onClick={(e) => e.stopPropagation()} className="fixed inset-0 z-50 top-16 mt-1 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <SideBar method={closeModal}/>
-                    </Disclosure.Panel>
-                  </Transition>
+                      <Transition as={Fragment} appear show={isOpen}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95">
+                        <Disclosure.Panel onClick={(e) => e.stopPropagation()} className="fixed inset-0 z-50 top-16 mt-1 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <SideBar method={closeModal} />
+                        </Disclosure.Panel>
+                      </Transition>
                     </>
                   ) : (
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
@@ -61,17 +61,17 @@ const Navbar: React.FC = () => {
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <a href="/user">
-                  <img
-                    className="block h-8 w-auto lg:hidden"
-                    src="/logo.png"
-                    alt="Logo"
-                  /></a>
-                  <a href="/user">
-                  <img
-                    className="hidden h-8 w-auto lg:block"
-                    src="/logo.png"
-                    alt="Logo"
-                  /></a>
+                    <img
+                      className="block h-8 w-auto lg:hidden"
+                      src="/logo.png"
+                      alt="Logo"
+                    /></a>
+                  <a href="/user" className="flex items-center">
+                    <img
+                      className="hidden h-8 w-auto lg:block"
+                      src="/logo.png"
+                      alt="Logo"
+                    />ChatOverflow</a>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -93,18 +93,18 @@ const Navbar: React.FC = () => {
                   >
                     <Menu.Items className="absolute right-1 z-10 mt-2 w-72 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
-                      <a href="#" className="flex items-center px-4 py-3 border-b hover:bg-gray-100">
+                        <a href="#" className="flex items-center px-4 py-3 border-b hover:bg-gray-100">
                           <p className="text-gray-600 text-sm mx-2">
                             <span className="font-bold">Arno</span> sended you a message · 2m
                           </p>
-                      </a>
+                        </a>
                       </Menu.Item>
                       <Menu.Item>
-                      <a href="#" className="flex items-center px-4 py-3 border-b hover:bg-gray-100">
+                        <a href="#" className="flex items-center px-4 py-3 border-b hover:bg-gray-100">
                           <p className="text-gray-600 text-sm mx-2">
                             <span className="font-bold">Gerald</span> sended you a message · 45m
                           </p>
-                      </a>
+                        </a>
                       </Menu.Item>
                     </Menu.Items>
                   </Transition>
@@ -115,7 +115,7 @@ const Navbar: React.FC = () => {
                   <div>
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
-                      <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"/>
+                      <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
                     </Menu.Button>
                   </div>
                   <Transition
@@ -167,5 +167,6 @@ const Navbar: React.FC = () => {
         </>
       )}
     </Disclosure>
-  )};
-  export default Navbar;
+  )
+};
+export default Navbar;
--- frontend/chatoverflow/pages/user/index.tsx
+++ frontend/chatoverflow/pages/user/index.tsx
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import SideBar from "@/components/Sidebar";
 
-const UserHome: React.FC<{chatChanged?: boolean}> = ({chatChanged}) => {
+const UserHome: React.FC<{ chatChanged?: boolean }> = ({ chatChanged }) => {
     const { data: session } = useSession();
 
 
@@ -9,19 +9,19 @@ const UserHome: React.FC<{chatChanged?: boolean}> = ({chatChanged}) => {
         // main container
         <div className='overflow-hidden'>
             {/* 2 components */}
-                <div className="flex flex-wrap bg-[#ffffff] h-screen">
-                    <div className="border min-w-[300px] max-w-[500] bg-gray-50 hidden sm:block">
-                        <SideBar/>
-                    </div>
-                    
-                    <div className="flex-grow w-min max-w-[1820px] py-4 p-2 md:p-4">
-                        <div className="flex justify-center">
-                            <h1 className="text-2xl mb-4">Welkom {session?.user.name} bij <span className="font-bold">ChatOverflow!</span></h1>
-                        </div>
+            <div className="flex flex-wrap bg-[#ffffff] h-screen">
+                <div className="border min-w-[300px] max-w-[500] bg-gray-50 hidden sm:block">
+                    <SideBar />
+                </div>
+
+                <div className="flex-grow w-min max-w-[1820px] py-4 p-2 md:p-4">
+                    <div className="flex justify-center">
+                        <h1 className="text-2xl mb-4">Welkom {session?.user.name} bij <span className="font-bold">ChatOverflow!</span></h1>
                     </div>
                 </div>
-                
             </div>
+
+        </div>
     );
 };
 
